5b57bc1c9123906e2d44dd5c7adc94ae1b0c46cf
krisztian666 <krisztian.karoczkai@gmail.com>
2023-10-27 11:30:57 +0200

    postgre connect

--- src/main/java/hu/yokudlela/functions/table/TableController.java
+++ src/main/java/hu/yokudlela/functions/table/TableController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController("table")
 @RequestMapping("/table")
 @Validated
+@CrossOrigin
 public class TableController {
     @Autowired
     private TableRepository repoTable;
